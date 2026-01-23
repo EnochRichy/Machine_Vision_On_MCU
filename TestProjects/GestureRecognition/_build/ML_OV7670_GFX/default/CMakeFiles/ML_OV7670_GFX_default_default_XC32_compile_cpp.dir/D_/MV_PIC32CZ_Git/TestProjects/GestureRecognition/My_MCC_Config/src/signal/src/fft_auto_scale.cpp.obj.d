aaeefcb4c5057d620fd5a8a0085547819ce32381
Enoch Richbert Jebakumar <enochrichbert.jebakumar@microchip.com>
2026-01-23 12:41:53 +0530

    Adding deletions

--- TestProjects/GestureRecognition/_build/ML_OV7670_GFX/default/CMakeFiles/ML_OV7670_GFX_default_default_XC32_compile_cpp.dir/D_/MV_PIC32CZ_Git/TestProjects/GestureRecognition/My_MCC_Config/src/signal/src/fft_auto_scale.cpp.obj.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/ML_OV7670_GFX_default_default_XC32_compile_cpp.dir/D_/MV_PIC32CZ_Git/TestProjects/GestureRecognition/My_MCC_Config/src/signal/src/fft_auto_scale.cpp.obj: \
- D:/MV_PIC32CZ_Git/TestProjects/GestureRecognition/My_MCC_Config/src/signal/src/fft_auto_scale.cpp \
- d:\mv_pic32cz_git\testprojects\gesturerecognition\my_mcc_config\src\signal\src\fft_auto_scale.h \
- c:\program\ files\microchip\xc32\v4.60\pic32c\include\musl\stddef.h \
- c:\program\ files\microchip\xc32\v4.60\pic32c\include\musl\bits\alltypes.h \
- c:\program\ files\microchip\xc32\v4.60\pic32c\include\musl\stdint.h \
- c:\program\ files\microchip\xc32\v4.60\pic32c\include\musl\bits\stdint.h \
- d:\mv_pic32cz_git\testprojects\gesturerecognition\my_mcc_config\src\signal\src\max_abs.h \
- d:\mv_pic32cz_git\testprojects\gesturerecognition\my_mcc_config\src\signal\src\msb.h
--- TestProjects/GestureRecognition/cmake/ML_OV7670_GFX/default/user.cmake
+++
@@ -1,3 +0,0 @@
-# Add include paths
-message(">>> user.cmake LOADED")
-add_compile_definitions(CMSIS_NN)
